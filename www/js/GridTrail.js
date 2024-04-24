f3c1afd5c5bac62a275d7c42e980510eafece44a
ApogeeS <63369176+ApogeeS@users.noreply.github.com>
2024-04-23 21:55:50 -0600

    Update GridTrail.js

--- www/js/GridTrail.js
+++ www/js/GridTrail.js
@@ -1,19 +1,30 @@
-const CELL_SIZE = 30; // size of each cell in the grid
+const patchExportURL = "https://cdn.jsdelivr.net/gh/apogees/www-js@latest/www/export/Soydan.export.json";
+const CELL_SIZE = 30;
 const COLOR_R = 143;
 const COLOR_G = 255;
 const COLOR_B = 0;
 const STARTING_ALPHA = 200;
 const BACKGROUND_COLOR = 0;
-const AMT_FADE_PER_FRAME = 20;
+const AMT_FADE_PER_FRAME = 15;
 
-// VARIABLES
 let probNeighbor = 0.0;
 let colorWithAlpha;
 let numRows;
 let numCols;
 let currentRow = -2;
 let currentCol = -2;
-let allNeighbors = []; // Array to store all neighbors
+let allNeighbors = [];
+
+let note = undefined;
+let mouseDownX = 0;
+let mouseDownY = 0;
+let device, context;
+
+async function setupRNBO() {
+    [device, context] = await createRNBODevice(patchExportURL);
+}
+
+setupRNBO();
 
 function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
@@ -25,99 +36,117 @@ function setup() {
   noFill();
   stroke(colorWithAlpha);
   strokeWeight(1);
-  numRows = Math.ceil(windowHeight / CELL_SIZE); // number of rows in the grid
-  numCols = Math.ceil(windowWidth / CELL_SIZE); // number of columns in the grid
+  numRows = Math.ceil(windowHeight / CELL_SIZE);
+  numCols = Math.ceil(windowWidth / CELL_SIZE);
 }
 
 function draw() {
   background(BACKGROUND_COLOR);
 
-  // Calculate the row and column of the cell that the mouse is currently over
   let row = floor(mouseY / CELL_SIZE);
   let col = floor(mouseX / CELL_SIZE);
 
-  // Check if the mouse has moved to a different cell
   if (row !== currentRow || col !== currentCol) {
     currentRow = row;
     currentCol = col;
-    // Add new neighbors to the allNeighbors array
     allNeighbors.push(...getRandomNeighbors(row, col));
   }
 
-  // Use the calculated row and column to determine the position of the cell
   let x = col * CELL_SIZE;
   let y = row * CELL_SIZE;
 
-  // Draw a highlighted rectangle over the cell under the mouse cursor
   stroke(colorWithAlpha);
   rect(x, y, CELL_SIZE, CELL_SIZE);
 
-  // Draw and update all neighbors
   for (let neighbor of allNeighbors) {
     let neighborX = neighbor.col * CELL_SIZE;
     let neighborY = neighbor.row * CELL_SIZE;
-    // Update the opacity of the neighbor
-    neighbor.opacity = max(0, neighbor.opacity - AMT_FADE_PER_FRAME); // Decrease opacity by 5 each frame
+
+    neighbor.opacity = max(0, neighbor.opacity - AMT_FADE_PER_FRAME);
     stroke(COLOR_R, COLOR_G, COLOR_B, neighbor.opacity);
     rect(neighborX, neighborY, CELL_SIZE, CELL_SIZE);
   }
-  // Remove neighbors with zero opacity
   allNeighbors = allNeighbors.filter((neighbor) => neighbor.opacity > 0);
+
+  if (device) {
+        const mouseDelta = Math.abs(mouseX - mouseDownX);
+        const mouseDeltaNormalized = map(mouseDelta, 0, width / 2, 0, 1);
+        const mouseDeltaY = Math.abs(mouseY - mouseDownY);
+        const mouseDeltaNormalizedY = map(mouseDeltaY, 0, height / 2, 0, 1);
+
+        const tremoloDepth = device.parametersById.get("poly/trem/depth");
+        tremoloDepth.normalizedValue = mouseDeltaNormalized;
+
+        const vibratoDepth = device.parametersById.get("poly/vibr/depth");
+        vibratoDepth.normalizedValue = mouseDeltaNormalizedY;
+
+        const tremoloRate = device.parametersById.get("poly/trem/rate");
+        tremoloRate.normalizedValue = mouseDeltaNormalized;
+
+        const vibratoRate = device.parametersById.get("poly/vibr/rate");
+        vibratoRate.normalizedValue = mouseDeltaNormalizedY;
+
+        const spread = device.parametersById.get("spread");
+        spread.normalizedValue = mouseDeltaNormalized;
+
+        const cutoff = device.parametersById.get("cutoff");
+        cutoff.normalizedValue = mouseDeltaNormalizedY;
+
+        const glide = device.parametersById.get("glide");
+        glide.normalizedValue = 0.2;
+    }
 }
 
 function getRandomNeighbors(row, col) {
-  let neighbors = []; // Initialize an empty array to store neighbor cells
+  let neighbors = [];
 
-  // Loop through the cells surrounding the given cell (row, col)
   for (let dRow = -1; dRow <= 1; dRow++) {
     for (let dCol = -1; dCol <= 1; dCol++) {
-      // Calculate the neighboring cell's row and column indices
       let neighborRow = row + dRow;
       let neighborCol = col + dCol;
 
-      // Check if the current cell in the loop is the given cell (row, col)
       let isCurrentCell = dRow === 0 && dCol === 0;
 
-      // Check if the neighboring cell is within the grid boundaries
       let isInBounds =
         neighborRow >= 0 &&
         neighborRow < numRows &&
         neighborCol >= 0 &&
         neighborCol < numCols;
 
-      // If the cell is not the given cell, is within bounds, and has a 50% chance,
-      // add the neighboring cell to the neighbors array
       if (!isCurrentCell && isInBounds && Math.random() < probNeighbor) {
         neighbors.push({
           row: neighborRow,
           col: neighborCol,
-          opacity: 255, // Initial opacity of the neighbor
+          opacity: 255,
         });
       }
     }
   }
-
-  // Return the array of randomly-selected neighboring cells
   return neighbors;
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  numRows = Math.ceil(windowHeight / CELL_SIZE); // number of rows in the grid
-  numCols = Math.ceil(windowWidth / CELL_SIZE); // number of columns in the grid
+  numRows = Math.ceil(windowHeight / CELL_SIZE);
+  numCols = Math.ceil(windowWidth / CELL_SIZE);
 }
 
 function mousePressed() {
   probNeighbor = 0.5;
+  mouseDownX = mouseX;
+  mouseDownY = mouseY;
+
+  if (device) {
+      context.resume();
+      note = map(mouseDownX, 0, width, 40, 80)
+      noteOn(device, context, note, 100);
+  }
 }
 
 function mouseReleased() {
   probNeighbor = 0.0;
-}
 
-// function mouseDragged() {
-//   if (sliding) {
-//     let newValue = map(mouseX, sliderX, sliderX + sliderWidth, 0, 100);
-//     sliderValue = constrain(newValue, 0, 100);
-//   }
-// }
+  if (device) {
+      noteOff(device, context, note);
+  }
+}
